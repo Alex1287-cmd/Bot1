2c824a703477b759af29b48ac0fda043f5f8e153
Alex1287-cmd <75934725+Alex1287-cmd@users.noreply.github.com>
2021-02-10 00:14:20 +0700

    Update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn }) => {
   if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 86400000) {
-    global.DATABASE._data.users[m.sender].exp += 1000
-    m.reply('+1000 XP')
-    global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
+    global.DATABASE._data.users[m.sender].exp += 5000
+    m.reply('+5000 XP sudah di claim coba lagi besok')
+    global.DATABASE._data.users[m.sender].lastclaim = new Date * 2
   } else m.reply('Anda sudah mengklaim klaim harian hari ini')
 }
 handler.help = ['daily', 'claim']
