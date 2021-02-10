c0a0e178175b3d49fb22ca88f421e714d166a1de
Alex1287-cmd <75934725+Alex1287-cmd@users.noreply.github.com>
2021-02-10 07:13:55 +0700

    Create lirik.js

---
+++ plugins/lirik.js
@@ -0,0 +1,6 @@
+if (text.includes(".lirik")){
+	const teks = text.split(".lirik")[1]
+	axios.get(`http://scrap.terhambar.com/lirik?word=${teks}`).then ((res) => {
+	     conn.sendMessage(id, '[WAIT] Searching...❗', MessageType.text)
+	 	let hasil = `🎶lirik🎶 lagu ${teks} \n\n\n ${res.data.result.lirik}`
+	conn.sendMessage(id, hasil, MessageType.text
