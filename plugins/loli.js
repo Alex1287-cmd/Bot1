e5abcd32351d8e7abe2d4b099dbea54ad63465ad
Alex1287-cmd <75934725+Alex1287-cmd@users.noreply.github.com>
2021-02-10 07:09:38 +0700

    Create loli.js

---
+++ plugins/loli.js
@@ -0,0 +1,15 @@
+var items = ["anime loli","anime loli sange","anime loli fackgirll","anime loli i love you"];
+    var nime = items[Math.floor(Math.random() * items.length)];
+    var url = "https://api.fdci.se/rep.php?gambar=" + nime;
+    
+    axios.get(url)
+      .then((result) => {
+        var n = JSON.parse(JSON.stringify(result.data));
+        var nimek =  n[Math.floor(Math.random() * n.length)];
+        imageToBase64(nimek) 
+        .then(
+            (response) => {
+	var buf = Buffer.from(response, 'base64'); 
+              conn.sendMessage(
+            id,
+              buf,MessageType.image
